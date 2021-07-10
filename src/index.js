b5d7cef95878a59afd31e4ff482dc3796f9d311e
Prince <avecillasprince@yahoo.com>
2021-07-10 19:15:07 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    change()
+    
+  })
 });
+
+
+
+function change() {
+  let li = document.createElement('li')
+  let description = document.querySelector('#new-task-description')
+  li.textContent = description.value
+  document.querySelector('#tasks').appendChild(li)
+
+  const text = li.textContent
+  console.log(text)
+  
+  
+}
